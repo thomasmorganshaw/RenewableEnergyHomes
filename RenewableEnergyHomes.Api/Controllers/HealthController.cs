eb4ff6782a66abc5d758741955f29d8eed8c5700
Thomas <thomas.morganshaw@gmail.com>
2022-08-17 19:07:14 +0100

    health endpoint

---
+++ RenewableEnergyHomes.Api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RenewableEnergyHomes.Api.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Healthy"
+            });
+        }
+    }
+}
